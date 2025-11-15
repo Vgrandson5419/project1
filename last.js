8bcd7f4b44b20a69c217fd1f3b8e09c7e7e0184c
Vgrandson <falmanstefan@gmail.com>
2025-11-15 14:54:33 +0200

    please be done

---
+++ last.js
@@ -0,0 +1,17 @@
+const numberofFilms = prompt('Как много фильмов вы ужк смотрели?' , '');
+
+const personalMovieDB = {
+    count:numberofFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?' , ''),
+      b = prompt('На сколько оцените его?' , ''),
+      c = prompt('Один из последних просмотренных фильмов?' , ''),
+      d = prompt('На сколько оцените его?' , '');   
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;

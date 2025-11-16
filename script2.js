c6a79370bcc747c9b7b1e753bfbb2d42f9454b86
Vgrandson <falmanstefan@gmail.com>
2025-11-16 19:35:45 +0200

    grind harder - functions, loops, conditions

---
+++ script2.js
@@ -0,0 +1,26 @@
+ 'use strict';
+ const numberofFilms = prompt('How much films have you seen ? ', "");
+
+
+ const personalMovieDB = {
+    count:numberofFilms, 
+    movie: {},
+    acors:{}, 
+    genres:[],
+    privat: false,
+ }
+
+ const a = ('One of the latest seen films :', '')
+       b = ('How much you can estimate it for :', '')
+       C = ('One of the latest seen films :', '')
+       d = ('How much you can estimate it for :', '')
+
+personalMovieDB.movies [a] = b ; 
+personalMovieDB.movies [c] = d; 
+ if (numberofFilms = " "){
+    prompt('Enter the number of films you have watched :', '')
+     }else if(str.length >50 ){
+        numberofFilms
+     }else{
+        numberofFilms
+     }
